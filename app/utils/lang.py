2c35e6eb72a47a95c7e08e5f7a93894bb9b28902
Kyryo <owenonions@gmail.com>
2021-09-11 07:52:49 +0200

    changed wording of the errors

--- app/utils/lang.py
+++ app/utils/lang.py
@@ -1,8 +1,8 @@
 error = {
-    "json_decode_error": "something went wrong while decoding the json",
-    "data_format": "wrong data format provided",
-    "invalid_phone": "phone number is not valid",
-    "disallowed_host": "unknown host",
-    "general": "something went wrong",
-    "authorization_error": "key not authorized",
+    "json_decode_error": "Something went wrong while decoding the json payload provided.",
+    "missing_required_keys": "Wrong data format provided. Some keys missing from the data provided.",
+    "invalid_phone": "The phone number provided is not valid.",
+    "disallowed_host": "Unknown host.",
+    "general": "Something went wrong. Please check the logs for hints of what happened.",
+    "authorization_error": "The key that you have used to make the call is not authorized.",
 }
