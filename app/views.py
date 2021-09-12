e27152cbe997c5a59278c490c3fdea7fc45f7052
Kyryo <owenonions@gmail.com>
2021-09-12 09:09:55 +0200

    added TODOs

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ import json
 
 @app.route('/v1/sms/send', methods=['POST'])
 def send_sms():
-    #! [TODO] it would be nice to check that the data coming in is really a JSON here
+    #! [TODO] would be nice to check that the data coming in is really a JSON
     data = json.loads(request.data)
 
     validator = validate.Validate(data)
@@ -15,4 +15,7 @@ def send_sms():
     if not validator.is_valid_phone_number():
         return json.dumps({"status": "error", "error": lang.error['invalid_phone']}), 400
 
+    #! [TODO] try to send the SMS
+    #! [TODO] check if the SMS was sent successfully
+    #! [TODO] return appropriate response
     return json.dumps({"status": "success", "message": "your message was sent successfully"}), 200
