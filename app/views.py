c2898c4bd7b8576de5bd8fe98d4ccce6ded1ce5a
Kyryo <owenonions@gmail.com>
2021-09-12 09:05:02 +0200

    added comment - add a check to confirm that data coming in is actually a json

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ import json
 
 @app.route('/v1/sms/send', methods=['POST'])
 def send_sms():
-
+    #! [TODO] it would be nice to check that the data coming in is really a JSON here
     data = json.loads(request.data)
 
     validator = validate.Validate(data)
@@ -16,3 +16,8 @@ def send_sms():
         return json.dumps({"status": "error", "error": lang.error['invalid_phone']}), 400
 
     return json.dumps({"status": "success", "message": "your message was sent successfully"}), 200
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True)
