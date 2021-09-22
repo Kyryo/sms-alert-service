788ff7e163fd9227cb1af09f5328967755a95dcc
Kyryo <owenonions@gmail.com>
2021-09-22 20:08:40 +0200

    removed test route and change todos to mere comments

--- app/views.py
+++ app/views.py
@@ -15,22 +15,16 @@ def send_sms():
     if not validator.is_valid_phone_number():
         return json.dumps({"status": "error", "error": lang.error['invalid_phone']}), 400
 
-    #! [TODO] try to send the SMS
+    # * try to send the SMS
     params = {}
     send_obj = send.Send(params)
 
-    #! [TODO] check if the SMS was sent successfully
+    # * check if the SMS was sent successfully
     if(send_obj.message()):
 
-        #! [TODO] return appropriate response
+        # * return appropriate response
         #! [TODO] when sending through twilio API, the API returns sid for each successful call - you can change in utils/send.py for the method to return the sid instead of bool
         return json.dumps({"status": "success", "message": "your message was sent successfully"}), 200
 
     else:
         return json.dumps({"status": "error", "message": "your message failed to send"}), 400
-
-
-@app.route('/', methods=['POST'])
-def test():
-    data = json.loads(request.data)
-    return json.dumps({"status": "success", "message": data}), 200
